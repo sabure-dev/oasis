da2aef727d7f13006bf83467da7cdd1e5e6d1b09
chlorine <dev.sabure@gmail.com>
2025-09-14 18:40:36 +0300

    Update docker.sh

--- backend/scripts/docker.sh
+++ backend/scripts/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd app
-uv run gunicorn proxy:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --access-logfile - --log-level info --timeout 120
+uv run gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --access-logfile - --log-level info --timeout 120
