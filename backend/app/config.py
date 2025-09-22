9f2eada1f7606987723ec1c2508a798af59d1024
sabure-dev <dev.sabure@gmail.com>
2025-09-22 21:56:24 +0300

    feat: migrate to new api domain & add secret headers

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,14 +2,15 @@ from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 PROJECT_ROOT = Path(__file__).parent.parent
 ENV_FILE_PATH = PROJECT_ROOT / ".env"
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=ENV_FILE_PATH, env_file_encoding="utf-8")
 
     DAB_API_URL: str
+    SECRET_USER_AGENT: str
 
 
 settings = Settings()
